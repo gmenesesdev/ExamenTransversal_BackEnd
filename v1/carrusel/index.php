2c80e1a96f2bc51e56e1bc9931ebe7f8e426e1ac
Gonzalo Meneses <gonzalo.menesesg@gmail.com>
2024-06-14 01:59:54 -0400

    Falta validar POST -> resolver hoy

--- v1/carrusel/index.php
+++ v1/carrusel/index.php
@@ -35,6 +35,113 @@ if ($_version == 'v1') {
                     echo json_encode(["Error" => "No tiene autorizacion GET"]);
                 }
                 break;
+            case 'POST':
+                if ($_header == $_token_post) {
+                    include_once 'controller.php';
+                    include_once '../conexion.php';
+                    $control = new Controlador();
+                    $objeto = json_decode(file_get_contents("php://input", true));
+                    $rs = $control->postNew($objeto);
+                    if ($rs) {
+                        http_response_code(201);
+                        echo json_encode(["data" => "Carrusel creado"]);
+                    } else if ($rs == false) {
+                        echo "ID ya existe";
+                    } else {
+                        http_response_code(409);
+                        echo json_encode(["Error" => "Error al crear Carrusel"]);
+                    }
+                } else {
+                    http_response_code(401);
+                    echo json_encode(["Error" => "No tiene autorizacion POST"]);
+                }
+                break;
+            case 'PATCH':
+                if ($_header == $_token_patch) {
+                    if ($existeId && $existeAccion) {
+                        include_once 'controller.php';
+                        include_once '../conexion.php';
+                        $control = new Controlador();
+                        if ($valorAccion == 'on') {
+                            $rs = $control->patchOnOff($valorId, 'true');
+                            http_response_code(200);
+                            echo json_encode(["data" => $rs]);
+                        } else if ($valorAccion == 'off') {
+                            $rs = $control->patchOnOff($valorId, 'false');
+                            http_response_code(200);
+                            echo json_encode(["data" => $rs]);
+                        } else {
+                            http_response_code(400);
+                            echo json_encode(["Error" => "Error con acciones"]);
+                        }
+                    } else {
+                        http_response_code(400);
+                        echo json_encode(["Error" => "Error con parametros"]);
+                    }
+                } else {
+                    http_response_code(401);
+                    echo json_encode(["Error" => "No tiene autorizacion PATCH"]);
+                }
+                break;
+
+            case 'PUT':
+                if ($header == $token_put) {
+                    include_once 'controller.php';
+                    include_once '../conexion.php';
+                    $control = new Controlador();
+                    $objeto = json_decode(file_get_contents("php://input", true));
+                    if ($objeto->imagen && !$objeto->titulo && !$objeto->descripcion) {
+                        $rs = $control->putImagenByID($objeto->imagen, $objeto->id);
+                        http_response_code(200);
+                        echo json_encode(["data" => $rs]);
+                    } else if (!$objeto->imagen && $objeto->titulo && !$objeto->descripcion) {
+                        $rs = $control->putTituloByID($objeto->titulo, $objeto->id);
+                        http_response_code(200);
+                        echo json_encode(["data" => $rs]);
+                    } else if (!$objeto->imagen && !$objeto->titulo && $objeto->descripcion) {
+                        $rs = $control->putDescripcionByID($objeto->descripcion, $objeto->id);
+                        http_response_code(200);
+                        echo json_encode(["data" => $rs]);
+                    } else {
+                        http_response_code(400);
+                        echo json_encode(["Error" => "Error con parametros"]);
+                    }
+                } else {
+                    http_response_code(401);
+                    echo json_encode(["Error" => "No tiene autorizacion PUT"]);
+                }
+                break;
+
+            case 'DELETE':
+                if ($_header == $_token_delete) {
+                    include_once 'controller.php';
+                    include_once '../conexion.php';
+                    $control = new Controlador();
+                    if ($existeId) {
+                        $rs = $control->getAll();
+                        $existeRegistro = false;
+                        foreach ($rs as $r) {
+                            if ($r['id'] == $valorId) {
+                                $existeRegistro = true;
+                            }
+                        }
+                        if ($existeRegistro) {
+                            $rs = $control->deleteByID($valorId);
+                            http_response_code(200);
+                            echo json_encode(["data" => $rs]);
+                        } else {
+                            http_response_code(404);
+                            echo json_encode(["data" => "no existe el registro"]);
+                        }
+                    } else {
+                        http_response_code(406);
+                        echo json_encode(["Error" => false]);
+                    }
+                } else {
+                    http_response_code(401);
+                    echo json_encode(["Error" => "No tiene autorizacion DELETE"]);
+                }
+                break;
             default:
                 http_response_code(405);
                 echo json_encode(["Error" => "No implementado"]);
